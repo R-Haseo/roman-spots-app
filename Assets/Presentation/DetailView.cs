ac1507de7e3a6fc6b974c322ac391042a3c6d2f0
R-Haseo <virtual.haseo20230107@gmail.com>
2026-07-01 22:48:16 +0900

    FEAT: add external link buttons to detail view

--- Assets/Presentation/DetailView.cs
+++ Assets/Presentation/DetailView.cs
@@ -3,6 +3,7 @@ using RomanSpots.Domain;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityApplication = UnityEngine.Application;
 
 namespace RomanSpots.Presentation
 {
@@ -11,7 +12,10 @@ namespace RomanSpots.Presentation
         [SerializeField] private TMP_Text _nameText;
         [SerializeField] private TMP_Text _infoText;
         [SerializeField] private Button _backButton;
+        [SerializeField] private Button _mapButton;
+        [SerializeField] private Button _referenceButton;
 
+        private Spot _spot;
         private Action _onBackClicked;
 
         public void Initialize(Action onBackClicked)
@@ -20,18 +24,33 @@ namespace RomanSpots.Presentation
 
             _backButton.onClick.RemoveAllListeners();
             _backButton.onClick.AddListener(() => _onBackClicked?.Invoke());
+
+            _mapButton.onClick.RemoveAllListeners();
+            _mapButton.onClick.AddListener(() => OpenUrl(_spot?.MapUrl));
+
+            _referenceButton.onClick.RemoveAllListeners();
+            _referenceButton.onClick.AddListener(() => OpenUrl(_spot?.ReferenceUrl));
         }
 
         public void SetData(Spot spot)
         {
+            _spot = spot;
             _nameText.text = spot.Name;
             _infoText.text =
                 $"{spot.Prefecture}\n" +
                 $"{spot.Category}\n\n" +
                 $"{spot.Description}\n\n" +
-                $"{spot.ImageAddress}\n\n" +
-                $"{spot.MapUrl}\n\n" +
-                $"{spot.ReferenceUrl}";
+                $"{spot.ImageAddress}\n\n";
+        }
+
+        private void OpenUrl(string url)
+        {
+            if (string.IsNullOrWhiteSpace(url))
+            {
+                return;
+            }
+
+            UnityApplication.OpenURL(url);
         }
     }
 }
